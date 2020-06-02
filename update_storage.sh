2e2a43b2690cc3c06f81853b478c31a5f6cdfbd8
Andrey Ryazantsev <andrey.ryazantsev@team.wrike.com>
2020-06-02 23:50:20 +0300

    .

--- update_storage.sh
+++ update_storage.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 echo "{}" | cat > ./resources/storage.json
+git config user.email "andrew.ryazantsev@gmail.com"
+git config user.name "Andrew Ryazantsev"
 git add ./resources/storage.json
 git commit -m 'update storage'
 git push
