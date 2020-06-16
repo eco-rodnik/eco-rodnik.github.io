530370958202042fd841950811cd4e4a8dfc2628
GitHub <noreply@github.com>
2020-06-16 22:55:29 +0300

    404 redirect

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import * as RodApp from './components/rod-app/rod-app.html';
 
 //import storageData from '../resources/storage.json';
 
+console.log('Redirected from:', sessionStorage.redirect);
+
 endorphin('rod-app', RodApp, {
     target: document.body
 });
